cd670568575e7f8bb286ac16f6e297d8d697c72d
Marcelo da Silva Nunes <msnunes.ti@gmail.com>
2022-10-19 20:18:19 -0300

    Iniciando

---
+++ src/main/java/com/example/copa2022/Copa2022Application.java
@@ -0,0 +1,13 @@
+package com.example.copa2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Copa2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Copa2022Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/copa2022/Copa2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.copa2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Copa2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
