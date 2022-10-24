9e736b66dfa3b7abbe26bf4cb6dcc657361af7cd
Marcelo da Silva Nunes <msnunes.ti@gmail.com>
2022-10-23 21:39:18 -0300

    Criado Entity Jogo

--- src/main/java/com/example/copa2022/models/Jogo.java
+++ src/main/java/com/example/copa2022/models/Jogo.java
@@ -1,4 +1,43 @@
 package com.example.copa2022.models;
 
+import com.sun.istack.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Entity(name = "jogos")
 public class Jogo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String chave;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "Selecao_mandante_id")
+    private Selecao selecaoMandante;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "selecao_visitante_id")
+    private Selecao selecaoVisitante;
+
+    @NotNull
+    private LocalDateTime dataHora;
+
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "estadio_id")
+    private Estadio estadio;
+
+    private Integer golsMandante;
+
+    private Integer golsVisitantes;
 }
---
+++ src/main/java/com/example/copa2022/repositories/JogoRepository.java
@@ -0,0 +1,8 @@
+package com.example.copa2022.repositories;
+
+import com.example.copa2022.models.Jogo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JogoRepository extends JpaRepository<Jogo, Long> {
+
+}
